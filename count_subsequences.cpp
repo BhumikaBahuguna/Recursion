f98331e6e79f8db07ba9fb97da0be5b1a7779467
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-07-30 09:39:50 +0530

    Add files via upload

---
+++ count_subsequences.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int printS(int ind,int s,int sum , int arr[],int n){
+    if(ind==n){
+        if(s==sum){
+            return 1;
+        }
+        else return 0;
+    }
+    s+=arr[ind];
+    int l=printS(ind+1,s,sum,arr,n) ;
+    s-=arr[ind];
+    int r=printS(ind+1,s,sum,arr,n) ;
+    return l+r;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int sum;
+    cin>>sum;
+    cout <<printS(0,0,sum,arr,n);
+    return 0;
+}
